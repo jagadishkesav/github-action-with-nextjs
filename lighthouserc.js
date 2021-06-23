68f4c409efeedaa1eb0431079da73fe33603c7c7
jagadishkesav <s.jagadishkpd@gmail.com>
2021-06-23 15:46:02 +0530

    new updates

--- lighthouserc.js
+++ lighthouserc.js
@@ -37,11 +37,6 @@ module.exports = {
       upload: {
         /* Add configuration here */
         target: 'temporary-public-storage',
-        urlReplacementPatterns: [
-          's/[0-9a-f]{12}$/HASH/',
-          's#:[0-9]{3,5}/#:PORT/#',
-          's/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/UUID/ig',
-        ],
       },
     },
   };
