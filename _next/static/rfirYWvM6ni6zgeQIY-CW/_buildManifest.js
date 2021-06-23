9aa21f84fbadad4fd4f56b7cee7194dd797ee477
jagadishkesav <46987467+jagadishkesav@users.noreply.github.com>
2021-06-23 03:41:19 +0000

    Deploying to master from @ jagadishkesav/github-action-with-nextjs@ab0ae623aa8c079b9446ce6da54acdfa02941e11 🚀

---
+++ _next/static/rfirYWvM6ni6zgeQIY-CW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/751-ac26adc2c5bbf3a9929e.js","static/chunks/pages/index-64d1bba3d0aaa464fa8b.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
