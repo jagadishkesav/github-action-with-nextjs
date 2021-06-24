99ce8966815476986dc063624fef8c89fe8c9327
jagadishkesav <s.jagadishkpd@gmail.com>
2021-06-24 12:28:42 +0530

    fix html lang

--- components/common/header.js
+++ components/common/header.js
@@ -9,13 +9,8 @@ const Header = () => {
                 name="viewport" 
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
-            <meta name="description" content="this description im using in github action using nextjs for testig purpose i have use so that i wont get errors " />
+            <meta name="description" content="This description im using in github action using nextjs for testig purpose i have use so that i wont get errors " />
             <title>Github Actions using Next Js</title>
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link 
-                rel="stylesheet" 
-                href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@200;300;400;500;600;700&display=swap" 
-            />
         </Head>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,12 @@
 import Header from "./common/header"
-import { Html } from "next/document"
 
 const Layout = ({ children }) => {
     return (
         <>
-            <Html lang="en">
             <Header />
             <main>
                 {children}
             </main>
-            </Html>
         </>
     )
 }
